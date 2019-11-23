251b0998df90270ca65d36760302c6e36acbbd77
Sabrina Rocha <delrock876@gmail.com>
2019-11-23 13:03:56 -0800

    animal & person model

---
+++ models/Person.js
@@ -0,0 +1,17 @@
+module.exports = (sequalize, Model, DataTypes) => {
+  class Person extends Model { }
+
+  Person.init({
+  name: DataTypes.STRING,
+  age: DataTypes.INTEGER,
+  address: DataType.STRING,
+  phone: DataTypes.STRING,
+  email: DataTypes.STRING,
+  petId: DataTypes.INTEGER
+  }, { sequelize, modelName: 'Person' })
+
+
+  return Person
+
+
+}
