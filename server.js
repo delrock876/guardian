5ade81a8e1708bbae26438450871c1c1cf4b5aad
E L A <ela@dhcp-v116-087.mobile.uci.edu>
2019-12-07 10:29:13 -0800

    updated readme

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.json())
 require('./routes')(app)
 
 db.sync()
-  .then(() => app.listen(process.env.PORT))
+  .then(() => app.listen(process.env.PORT || 3000))
   .catch(e => console.error(e))
