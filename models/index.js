967bb20fedb570dbbe3fd9598c03222d1ca13479
Bryant Nguyen <bryant_72002@yahoo.com>
2019-12-08 20:06:13 -0800

    merging with master

--- models/index.js
+++ models/index.js
@@ -6,5 +6,6 @@ const Animal =  require('./animal.js')(sequelize, Model, DataTypes)
 const Person = require('./Person.js')(sequelize, Model, DataTypes)
 
 Person.hasOne(Animal)
+// Animal.hasOne(Person)
 
 module.exports =  {Animal, Person}
