4a8be406613b7d8a888fd6b30aafae6f2e52a277
CydVicious3 <53547068+CydVicious3@users.noreply.github.com>
2019-12-10 18:25:14 -0800

    Revert "styling and association display on adopted page"

--- models/index.js
+++ models/index.js
@@ -6,6 +6,5 @@ const Animal =  require('./animal.js')(sequelize, Model, DataTypes)
 const Person = require('./Person.js')(sequelize, Model, DataTypes)
 
 Animal.hasOne(Person)
-Person.belongsTo(Animal)
 
 module.exports =  {Animal, Person}
